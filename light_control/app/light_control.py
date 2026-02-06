0ba6f23fd78081f46bac0d43134e904005791560
Bill Leddy <bill@williesworkshop.net>
2026-02-05 17:50:54 -0800

    latest version of micropython files

--- light_control/app/light_control.py
+++ light_control/app/light_control.py
@@ -1,24 +1,23 @@
 # Based on Rui Santos & Sara Santos - Random Nerd Tutorials
 # Complete project details at https://RandomNerdTutorials.com/raspberry-pi-pico-w-asynchronous-web-server-micropython/
 
-import uasyncio as asyncio
+import asyncio
 import time
-import sys
 import json
 from machine import Pin
 from ntp_clock import Clock
 from wifi_connect import connection
 from os_path import make_path
+from logging import logging as log
 
-
-if sys.platform == 'esp32':
-    led = Pin(2, Pin.OUT)
-else:
-    led = Pin('LED', Pin.OUT)
+led = Pin(26, Pin.OUT)
 led.off()
+on_board_led = Pin('LED',Pin.OUT)
     
-relay_pin = Pin(2, Pin.OUT)
+relay_pin = Pin(10, Pin.OUT) # was 15 ... 15 may be toasted?
+log.debug(f'Startup relay_pin: {relay_pin.value()}')
 relay_pin.off()
+push_button = Pin(14,Pin.IN,Pin.PULL_UP)
 
 connection.connect()
 
@@ -29,18 +28,68 @@ except:
     pass
 
 DATA_FILE = "instance/lights.json"
-delay_time = 0
 
-def acknowledge(times=2):
-    # even times returns led to prev state
-    for x in range(times):
-        led.toggle()
-        time.sleep(0.5)
+## Some Globals...
+delay_time = -1
+blink_delay = 0.5
+blink_times = 0
+button_buffer_ticks = time.ticks_ms()
+
+def set_led(val):
+    # keep the onboard and external leds in syinc
+    led.value(val)
+    on_board_led.value(val)
+
+
+async def blink():
+    global blink_times, blink_delay
+    val = led.value()
+    blink_times = int(blink_times)
+    while blink_times > 0:
+        for z in range(2):
+            val = 1 if val == 0 else 0
+            set_led(val)
+            log.debug(f'Blink = Relay: {relay_pin.value()} LED: {led.value()}')
+            await asyncio.sleep(blink_delay)
+        blink_times -= 1
+    set_led(relay_pin.value())
+    # reset to defaults
+    blink_delay = 0.5
+    blink_times = 0
+
+
+async def handle_button():
+    global blink_times, blink_delay, button_buffer_ticks
+    button_up = push_button.value()
+    if button_up or button_buffer_ticks > time.ticks_ms():
+        return # wait between button presses
     
-acknowledge(4)
+    current_state = button_up
+    light_on = (relay_pin.value() == 1)
+    time.sleep(0.05)
+    button_up = push_button.value()
+    log.debug(f'Relay: {relay_pin.value()} LED: {led.value()} Light_on: {light_on}')
+    if not button_up and button_up == current_state:
+        print('button pressed')
+        data = json.loads(get_status_from_file())
+        val = 0 if led.value() == 1 else 1
+        set_led(val)
 
-async def manage_switch():
-    global delay_time
+        if light_on:
+            data['mode'] = 3 # off till next
+        else:
+            data['mode'] = 2 # on till next
+         
+        # used to delay a bit before processing another press
+        button_buffer_ticks = time.ticks_ms() + 2000
+
+        # save the new status
+        log.info(f'Button pressed, Mode: {data["mode"]}')
+        save_status_to_file(json.dumps(data))
+        
+
+async def manage_mode():
+    global delay_time, blink_delay, blink_times, relay_pin
     try:
         if not connection.is_connected():
             connection.connect()
@@ -83,22 +132,26 @@ async def manage_switch():
     
     if light_on:
         delay_time = 0
-        led.on()
-        relay_pin.on()
+        relay_pin.value(1)
+        log.debug(f'manage_mode = (Turn on light) Relay: {relay_pin.value()}, light_on: {light_on}')
     else:
-    #   print('delay_time:',delay_time,'Time:',time.time())
+#         print('delay_time:',delay_time,'Time:',time.time())
         if delay_time == 0:
             delay_time = time.time() + data.get('delay_seconds',0)
     #       print('delay_time set to:',delay_time,'Time:',time.time())
-        if time.time() >= delay_time:
-            led.off()
+            blink_delay = 1
+            blink_times = data.get('delay_seconds',0)
+
+        elif time.time() >= delay_time:
             relay_pin.off()
-        
+            
     # save the data?
     if new_mode != data.get('mode'):
-        print('saving:',data)
+        data['mode'] = new_mode
+        log.info(f'saving: {data}')
         save_status_to_file(json.dumps(data))
         
+    log.debug(f'manage_mode = Relay: {relay_pin.value()} LED: {led.value()} Light_on: {light_on}')
 
 def save_status_to_file(data):
     make_path('/',DATA_FILE)
@@ -116,7 +169,7 @@ def get_status_from_file():
     
     if not content:
         content = default_content
-        print("using default data")
+        log.info("using default data")
 
     return content
 
@@ -134,14 +187,21 @@ def decode(data):
     return data
 
 def status():
-    acknowledge()
+    global blink_times, blink_delay
+    blink_times = 2
     content = get_status_from_file()
 #     print(f"handle status request: {content}")
 
     return content
 
 def update(data):
-    acknowledge()
+#     global blink_times, blink_delay
+#     blink_times = 2
+
+##### TODO #####
+# This needs to be more robust to validate the data
+###############################
+
     if data:        
         save_status_to_file(data)
         content = 'Ok'
@@ -158,14 +218,14 @@ async def handle_client(reader, writer):
     
     print("Client connected")
     request_line = await reader.readline()
-#     print('Request:', request_line)
     
     # Skip HTTP request headers
     while await reader.readline() != b"\r\n":
         pass
     
     request = str(request_line, 'utf-8').split()[1]
-#     print('Request:', request)
+    
+    response = ''
     
     if request == '/lights/status.json':
         response = status()
@@ -173,28 +233,28 @@ async def handle_client(reader, writer):
         data = ''
         tmp = request.split('?')
         if len(tmp) > 1:
-            data = decode(tmp[1])
-#             print('data received: '+ data)
-
-        response = update(data)
-    else:
-        response = ''
+            try:
+                data = decode(tmp[1])
+                response = update(data)
+            except:
+                pass
         
     if response:
         # Send the HTTP response and close the connection
         writer.write('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
-        writer.write(response)
-        await writer.drain()
     else:
         writer.write('HTTP/1.0 404 NOT FOUND\r\nContent-type: text/html\r\n\r\n')
-        writer.write('Page Not Found')
-        await writer.drain()
+        response = 'Page Not Found'
         
+    writer.write(response)
+    await writer.drain()
+
     await writer.wait_closed()
     print('Client Disconnected')
     
 
-async def main():    
+async def main():
+    global blink_times
     if not connection.is_connected():
         connection.connect()
     if not rtc.has_time:
@@ -204,12 +264,21 @@ async def main():
     print('Setting up server')
     server = asyncio.start_server(handle_client, "0.0.0.0", 80)
     asyncio.create_task(server)
-    asyncio.create_task(manage_switch())
+    asyncio.create_task(manage_mode())
+    asyncio.create_task(handle_button())
+    asyncio.create_task(blink())
+    
+    blink_times = 2
     
     while True:
-        # Add other tasks that you might need to do in the loop
-        await asyncio.sleep(1)
-        await manage_switch()
+        try:
+            # Add other tasks that you might need to do in the loop
+            await asyncio.sleep(1) # a delay is needed to give server some time to work
+            await manage_mode()
+            await blink()
+            await handle_button()
+        except Exception as e:
+            log.error(f'Main loop error: {str(e)}')
 
 # Create an Event Loop
 loop = asyncio.get_event_loop()
--- light_control/app/main.py
+++ light_control/app/main.py
@@ -1,3 +1,5 @@
+from logging import logging as log
 
+log.setLevel(log.INFO)
 
 import light_control
